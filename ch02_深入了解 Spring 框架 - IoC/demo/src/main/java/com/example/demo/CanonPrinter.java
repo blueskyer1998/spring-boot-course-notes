28337bbb11f7c1c95bd40b7061e747f114a646d7
blueskyer1998 <litpm1998@gmail.com>
2025-12-07 18:03:08 +0800

    feat(ch2): 新增 Spring IoC 範例程式
    - Bean 的注入 - @Autowired、@Qualifier
    - Bean 的創建 - @Component、@Configuration + @Bean

--- ch02_深入了解 Spring 框架 - IoC/demo/src/main/java/com/example/demo/CanonPrinter.java
+++ ch02_深入了解 Spring 框架 - IoC/demo/src/main/java/com/example/demo/CanonPrinter.java
@@ -2,10 +2,10 @@ package com.example.demo;
 
 import org.springframework.stereotype.Component;
 
-//@Component
-public class HpPrinter implements Printer {
+@Component
+public class CanonPrinter implements Printer {
     @Override
     public void print(String message) {
-        System.out.println("HP印表機: " + message);
+        System.out.println("Canon印表機: " + message);
     }
 }
